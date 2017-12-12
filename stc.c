8d8f7e9864217c9340e0ece223f498b0d7818014
Nandhini M <30834130+NandhiniManian@users.noreply.github.com>
2017-12-12 15:13:13 +0530

    Rename st.c to stc.c

---
+++ stc.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+char a[30],b[30];
+int i=0;
+printf("Enter the two strings:\n");
+gets(a);
+gets(b);
+while(a[i]==b[i]&&a[i]!='\0')
+
+i++;
+if(a[i]>b[i])
+printf("a>b");
+else if(a[i]<b[i])
+printf("a<b");
+else
+printf("a=b");
+
+}
